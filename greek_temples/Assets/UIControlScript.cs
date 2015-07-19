c8b4c26e9136baa0cc27343e428bb4fa05398316
Chris Shaw <cshaw1633@gmail.com>
2015-07-19 01:15:49 -0400

    assets for everything EXCEPT column capitals created and added

---
+++ greek_temples/Assets/UIControlScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIControlScript : MonoBehaviour {
+
+	public GameObject ColumnZero;
+	public GameObject ColumnSixteen;
+	public GameObject ColumnTwenty;
+	public GameObject ColumnTwentyFour;
+
+	public GameObject Pediment;
+	public GameObject Roof;
+	public GameObject CellaWalls;
+	public GameObject TempleBase;
+
+	public GameObject CapitalDoric;
+	public GameObject CapitalIonic;
+	public GameObject CapitalCorinthian;
+
+	//public GameObject entablature?  Have to figure that out.  
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
